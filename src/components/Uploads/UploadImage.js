896a78e909bc3696087a7de38466e2ee79ae0ee1
Tomohiko <pasteblock2021@gmail.com>
2022-02-26 13:19:58 -0500

    camera

--- src/components/Uploads/UploadImage.js
+++ src/components/Uploads/UploadImage.js
@@ -28,7 +28,7 @@ export default function UploadImage(props) {
   }, []);
 
   const addImage = async () => {
-    let _image = await ImagePicker.launchImageLibraryAsync({
+    let _image = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 3],
